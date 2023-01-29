7c3f888a881007e461ed1e2f98d184627c6bddb8
anjinsan132131 <anjinsan132131@gmail.com>
2023-01-29 20:29:18 +0300

    feat: create header, footer and routing

---
+++ src/constans.ts
@@ -0,0 +1,20 @@
+export enum AppRoute {
+  Login = '/login',
+  Main = '/',
+  Settings = '/settings'
+}
+
+export const DEVELOPERS = [
+  {
+    name: 'Ivanou Andrei',
+    linkPath: 'https://github.com/anjinsan132131'
+  },
+  {
+    name: 'Alexander Mikheev',
+    linkPath: 'https://github.com/solidrules'
+  },
+  {
+    name: 'Lev Sylin',
+    linkPath: 'https://github.com/Levosilimo'
+  }
+];
---
+++ src/pages/login-page/login-page.tsx
@@ -0,0 +1,14 @@
+import Footer from "../../components/footer/footer";
+import Header from "../../components/header/header";
+
+function LoginPage () {
+  return (
+    <div className="page page-login">
+      <Header/>
+      <p>Login Page</p>
+      <Footer/>
+    </div>
+  )
+}
+
+export default LoginPage;
