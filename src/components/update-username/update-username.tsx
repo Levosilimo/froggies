b45054190bc5a1f10f7fee6926302d4bb3cea6ef
SolidRules <amikheev1993@gmail.comm>
2023-02-25 20:16:31 +0300

    feat: add edit icons

---
+++ src/components/update-username/update-username.tsx
@@ -0,0 +1,28 @@
+
+function addInput(elem: HTMLParagraphElement) {
+  const input = document.createDocumentFragment();
+  const nameInput = document.createElement('input');
+  nameInput.type = 'text';
+  nameInput.placeholder = 'Enter new username';
+  // nameInput.className = 'name-input';
+  const passwordInput = document.createElement('input');
+  passwordInput.type = 'password';
+  passwordInput.placeholder = 'Enter your password';
+  input.append(nameInput, passwordInput);
+  elem.firstElementChild?.replaceWith(input);
+}
+
+function addName(elem: HTMLElement) {
+  const name = document.createElement('span');
+  elem.firstElementChild?.replaceWith(name);
+}
+
+function updateName(name: string, password: string) {
+
+}
+
+// function changeName(elem:HTMLElement) {
+//   addInput(elem);
+
+//   addName(elem);
+// }
